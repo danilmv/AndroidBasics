5e6ac7a5f8520fea3e23f16cb6d536f52c1f51bc
danilmv <43599357+danilmv@users.noreply.github.com>
2021-04-16 19:46:51 +0300

    MaterialDesign new enhanced EditText SnackBar CoordinatorLayout AppBarLayout CollapsingToolbarLayout Toolbar FloatingActionButton
    get bored and didn't finish :)

--- lesson7/src/main/java/com/andriod/androidbasics/lesson7/MainActivity.java
+++ lesson7/src/main/java/com/andriod/androidbasics/lesson7/MainActivity.java
@@ -1,14 +1,49 @@
 package com.andriod.androidbasics.lesson7;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextInputEditText text = findViewById(R.id.text_input_text);
+        text.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) return;
+            TextView t = (TextView) v;
+
+            if (!t.getText().toString().equals("текст")) {
+                t.setError("Введите 'текст'");
+            } else
+                t.setError(null);
+        });
+
+        toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        // Обработка нажатия на плавающую кнопку
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Здесь вылетит Snackbar
+                Snackbar.make(view, "Вы нажали на плавающую кнопку", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
     }
 }
